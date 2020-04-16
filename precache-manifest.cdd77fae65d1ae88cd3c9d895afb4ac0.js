1c5bf597370640ccf778c2e0d35d82b6078e5fb9
vineet <vineet.agrawal@47billion.com>
2020-04-16 16:34:02 +0530

    Updates

---
+++ precache-manifest.cdd77fae65d1ae88cd3c9d895afb4ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f059bb28c77ea47843a1a39b26ea3f",
+    "url": "/react-redux-demo/index.html"
+  },
+  {
+    "revision": "0d2387ac943b778c17bb",
+    "url": "/react-redux-demo/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "78a00c859d0e615207b4",
+    "url": "/react-redux-demo/static/js/2.57958418.chunk.js"
+  },
+  {
+    "revision": "0d2387ac943b778c17bb",
+    "url": "/react-redux-demo/static/js/main.a1bfb2e6.chunk.js"
+  },
+  {
+    "revision": "73e91e480878c6ea379d",
+    "url": "/react-redux-demo/static/js/runtime~main.d0406115.js"
+  }
+]);
